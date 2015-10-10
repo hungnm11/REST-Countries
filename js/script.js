0c279a3d630a6f19aec96b1f796b9b8ca8d273fe
hungnm11 <hung.nguyen@hotmail.com.vn>
2015-10-10 10:09:43 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
+
 $(document).ready(function(){
-	$.ajax({
-		url: 'https://restcountries.eu/rest/v1/all',
-		type: "GET",
-		dataType: "json",
-		success: function(data) {
-			console.log(data);
-		}
+	
+	var url = 'https://restcountries.eu/rest/v1/all';
+	$.getJSON(url, function(data) {
+		drawTable(data);
 	});
+	
+	function drawTable(data) {
+		for (var i = 0; i < data.length; i++) {
+			drawRow(data[i], i+1);
+		}
+	}
+	
+	function drawRow(rowData, id) {
+		var row = $("<tr/>");
+		$(".table").append(row);
+		row.append($("<td>" + id + "</td>"));
+		row.append($("<td>" + rowData.name + "</td>"));
+		row.append($("<td>" + rowData.altSpellings[0] + "</td>"));
+		row.append($("<td>" + rowData.capital + "</td>"));
+		row.append($("<td>" + rowData.region + "</td>"));
+		row.append($("<td>" + rowData.population + "</td>"));
+	}
 });
+
+
+
+
