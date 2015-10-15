e2936a29243647f8ecac902e8fb81878a2bfb52f
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2015-10-15 16:19:58 +0700

    update paging

--- js/script.js
+++ js/script.js
@@ -2,56 +2,47 @@
 $(document).ready(function(){
 	
 	var url = 'https://restcountries.eu/rest/v1/all';
-    var jqxhr = $.getJSON(url, function(data) {
     var $region = "undefined";
-        
+    var $countRowTotal = 0;
+    var $rowsTotal = 0
+    
+    
+    var jqxhr = $.getJSON(url, function(data) {
+    
         drawTable(data);
         
 	}).success(function(data) {
         
-        $(".dropdown-menu li").click(function() {
+        var $rowsShown = 15;
+            $rowsTotal = $('.table tbody tr').length;
+        
+        $(".dropdown-menu li").click(function(e) {
+            
             var $region = $(this).text();
             var $node = $("#dropdownMenu1").contents().first()[0].nodeValue = $region + " ";
-            console.log($region);
+            
+            
+            
             if ($region) {
-                $(".table").find("td:not(:contains(" + $region + "))").parent().slideUp();
-                $(".table").find("td:contains(" + $region + ")").parent().slideDown();
+                var rowNotShow = $(".table").find("td:not(:contains(" + $region + "))").parent().slideUp();
+                var rowShow = $(".table").find("td:contains(" + $region + ")").parent().slideDown();
+                $rowsTotal = rowShow.length;
+                if ($region == "All Region") {
+                    $region = false;
+                    console.log($rowsTotal);
+                }
+                console.log($region);
+                pagination($rowsTotal, $region)
             }
             
-            if ($region == "All Region") {
-                $(".table tr").slideDown();
-            }
+            
             
         });
         
         
-        function pagination() {
-            $('.table').after('<div id="nav"><ul class="pagination"></ul></div>');
-                var rowsShown = 25;
-                var rowsTotal = $('.table tbody tr').length;
-
-                var numPages = rowsTotal/rowsShown;
-                for(i = 0; i < numPages; i++) {
-                    var pageNum = i + 1;
-                    $('#nav ul').append('<li><a href="#" rel="'+i+'">'+pageNum+'</a></li> ');
-                }
-                $('.table tbody tr').hide();
-                $('.table tbody tr').slice(0, rowsShown).show();
-                $('#nav a:first').addClass('active');
-                $('#nav a').bind('click', function(){
-
-                    $('#nav a').removeClass('active');
-                    $(this).addClass('active');
-                    var currPage = $(this).attr('rel');
-                    var startItem = currPage * rowsShown;
-                    var endItem = startItem + rowsShown;
-                    $('.table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
-                            css('display','table-row').animate({opacity:1}, 300);
-            });
-        }
+        pagination($rowsTotal);
+	   console.log($rowsTotal);
         
-        pagination();
-	
     }).fail(function() {
         console.log("error");
     });
@@ -60,11 +51,12 @@ $(document).ready(function(){
         console.log("third success");
     });
 	
-    
 	function drawTable(data) {
-		for (var i = 0; i < data.length; i++) {
-            drawRow(data[i], i+1);
+        var $rowsTotal = data.length;
+		for (var i = 0; i < $rowsTotal; i++) {
+                drawRow(data[i], i+1);
 		}
+        
 	}
 	
 	function drawRow(rowData, id) {
@@ -78,7 +70,46 @@ $(document).ready(function(){
 		row.append($("<td>" + rowData.population + "</td>"));
 	}
 	
-	
+	function pagination($rowsTotal, $region) {
+        console.log($rowsTotal);
+        $('#nav').remove();
+        $('.table').after('<div id="nav"><ul class="pagination"></ul></div>');
+            var rowsShown = 15;
+            var rowsTotal = $rowsTotal;
+
+            var numPages = rowsTotal/rowsShown;
+            for(i = 0; i < numPages; i++) {
+                var pageNum = i + 1;
+                $('#nav ul').append('<li><a href="#" rel="'+i+'">'+pageNum+'</a></li> ');
+            }
+        
+            if ($region) {
+                $(".table").find("td:not(:contains(" + $region + "))").parent().slideUp();
+                $(".table").find("td:contains(" + $region + ")").parent().slice(0, rowsShown).slideDown();
+            } else {
+                $('.table tbody tr').hide();
+                $('.table tbody tr').slice(0, rowsShown).show();
+                $('#nav a:first').addClass('active');
+            }
+        
+            $('#nav a').bind('click', function(){
+                
+                $('#nav a').removeClass('active');
+                $(this).addClass('active');
+                var currPage = $(this).attr('rel');
+                var startItem = currPage * rowsShown;
+                var endItem = startItem + rowsShown;
+                var regionRows = $(".table").find("td:not(:contains(" + $region + "))");
+
+                if ($region && $('.table tbody tr').css('display') == 'table-row') {
+                    $(".table").find("td:contains(" + $region + ")").parent().slideUp().slice(startItem, endItem).slideDown();
+                } else {
+                    $('.table tbody tr').slideUp().slice(startItem, endItem).slideDown();
+//                    $('.table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+//                        css('display','table-row').animate({opacity:1}, 300);
+                }
+        });
+    }
 });
 
 
