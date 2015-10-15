6e5a1de8d738b93d0d74770fea4cfc6c58951692
hungnm11 <hung.nguyen@hotmail.com.vn>
2015-10-15 21:29:29 +0700

    update previous button

--- js/script.js
+++ js/script.js
@@ -21,8 +21,12 @@ $(document).ready(function(){
             var $region = $(this).text();
             var $node = $("#dropdownMenu1").contents().first()[0].nodeValue = $region + " ";
             
-            if ($region) {
-				console.log($region);
+			if ($region == 'All Region') {
+				$('.table tbody tr').show();
+				$('.table tbody tr').animate({opacity:1}, 300);
+				var $rowsTotal = $('.table tbody tr').length;
+				 pagination($rowsTotal);
+			} else {
                 $(".table").find("td:not(:contains(" + $region + "))").parent().hide();
 				$(".table").find("td:not(:contains(" + $region + "))").parent().animate({opacity:1}, 300);
                 var rowShow = $(".table").find("td:contains(" + $region + ")").parent().show();
@@ -66,42 +70,45 @@ $(document).ready(function(){
 
 		$('#nav').remove();
         $('.table').after('<div id="nav"><ul class="pagination"></ul></div>');
-            var rowsShown = 15;
-            var rowsTotal = $rowsTotal;
+		var rowsShown = 15;
+		var rowsTotal = $rowsTotal;
 
-            var numPages = rowsTotal/rowsShown;
-            for(i = 0; i < numPages; i++) {
-                var pageNum = i + 1;
-                $('#nav ul').append('<li><a href="#" rel="'+i+'">'+pageNum+'</a></li> ');
-            }
-        
-            if ($region) {
-                $(".table").find("td:not(:contains(" + $region + "))").parent().hide();
-                $(".table").find("td:contains(" + $region + ")").parent().slice(0, rowsShown).show();
-				$('#nav a:first').addClass('active');
-            } else {
-                $('.table tbody tr').hide();
-                $('.table tbody tr').slice(0, rowsShown).show();
-                $('#nav a:first').addClass('active');
-            }
-        
-            $('#nav a').bind('click', function(){
-                
-                $('#nav a').removeClass('active');
-                $(this).addClass('active');
-                var currPage = $(this).attr('rel');
-                var startItem = currPage * rowsShown;
-                var endItem = startItem + rowsShown;
-                var regionRows = $('.table tbody tr').css('display');
-				
-                if ($region) {
-					console.log($region);
-					$(".table").find("td:not(:contains(" + $region + "))").parent().hide();
-                    $(".table").find("td:contains(" + $region + ")").parent().css('opacity','0.0').hide().slice(startItem, endItem).css('display','table-row').animate({opacity:1}, 300);
-                } else {
-                    $('.table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
-                        css('display','table-row').animate({opacity:1}, 300);
-                }
+		var numPages = rowsTotal/rowsShown;
+		for(i = 0; i < numPages; i++) {
+			var pageNum = i + 1;
+			$('#nav ul').append('<li><a href="#" rel="'+i+'">'+pageNum+'</a></li> ');
+		}
+
+		$('#nav ul').prepend('<li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>');
+		
+		if ($region) {
+			$(".table").find("td:not(:contains(" + $region + "))").parent().hide();
+			$(".table").find("td:contains(" + $region + ")").parent().slice(0, rowsShown).show();
+			$('#nav li:eq(1)').addClass('active');
+		} else {
+			$('.table tbody tr').hide();
+			$('.table tbody tr').slice(0, rowsShown).show();
+			$('#nav li:eq(1)').addClass('active');
+		}
+
+		$('#nav a').bind('click', function(){
+
+			$('#nav a').parent().removeClass('active');
+			$(this).parent().addClass('active');
+			
+			var currPage = $(this).attr('rel');
+			var startItem = currPage * rowsShown;
+			var endItem = startItem + rowsShown;
+			var regionRows = $('.table tbody tr').css('display');
+
+			if ($region) {
+				console.log($region);
+				$(".table").find("td:not(:contains(" + $region + "))").parent().hide();
+				$(".table").find("td:contains(" + $region + ")").parent().css('opacity','0.0').hide().slice(startItem, endItem).css('display','table-row').animate({opacity:1}, 300);
+			} else {
+				$('.table tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+					css('display','table-row').animate({opacity:1}, 300);
+			}
         });
     }
 });
